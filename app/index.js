33ed788813afc5d43b975966a1759988ee23d03a
Alex Raduca <alexandru.raduca@gmail.com>
2017-08-28 15:54:33 +0300

    Homescreen: Container: done

--- app/index.js
+++ app/index.js
@@ -1,21 +1,10 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import EStyleSheet from "react-native-extended-stylesheet";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+import Home from "./screens/Home";
+
+EStyleSheet.build({
+  $primaryBlue: "#4F6D7A"
 });
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Let's build a currency converter app from app/index.js!</Text>
-      </View>
-    );
-  }
-}
+export default () => <Home />;
